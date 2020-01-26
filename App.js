01d6cd0060cb1d1d88ca434ca9a95c9c547f59c6
A Richardson <g.andrerichards@gmail.com>
2020-01-26 00:56:23 -0500

    proj setup

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from "expo"
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'openSans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'openSansBold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    )
+  }
+
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
