b59250f6aa247048fb52581bc5e3b1c857ac63b1
A Richardson <g.andrerichards@gmail.com>
2020-01-27 23:27:09 -0500

    add recipes class

---
+++ models/recipes.js
@@ -0,0 +1,32 @@
+class Recipes {
+  constructor(
+    id,
+    categoryID,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree) {
+    this.id = id
+    this.categoryID = categoryID
+    this.title = title
+    this.affordability = affordability
+    this.complexity = complexity
+    this.imageUrl = imageUrl
+    this.duration = duration
+    this.ingredients = ingredients
+    this.steps = steps
+    this.isGlutenFree = isGlutenFree
+    this.isVegan = isVegan
+    this.isVegetarian = isVegetarian
+    this.isLactoseFree = isLactoseFree
+  }
+}
+
+export default Recipes
