97bba6e14c7ed6bcbc3252192d75cc4d0026eb94
A Richardson <g.andrerichards@gmail.com>
2020-01-30 01:50:04 -0500

    style meal items

---
+++ components/MealItem.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
+import Colors from '../constants/Colors'
+
+const MealItem = ({ data, onSelectMeal }) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground source={{ uri: data.imageUrl }} style={styles.bgImage}>
+              <View style={styles.titleContainer}>
+                <Text numberOfLines={1} style={styles.title}>{data.title}</Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <Text>{data.duration}mins</Text>
+            <Text>{data.complexity.toUpperCase()}</Text>
+            <Text>{data.affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: '96%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginVertical: 5,
+    overflow: 'hidden',
+    alignSelf: 'center'
+  },
+  mealRow: {
+    flexDirection: 'row'
+  },
+  mealHeader: {
+    height: '85%'
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '15%'
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontFamily: 'openSansBold',
+    fontSize: 22,
+    color: '#fff',
+    textAlign: 'center'
+  },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end'
+  }
+})
+
+export default MealItem
--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
-import Colors from '../constants/Colors'
 import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
+import Colors from '../constants/Colors'
 
 const CategoryMealScreen = props => {
   const renderMealItem = itemData => {
-    console.log(itemData)
-    return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
-    )
+    return <MealItem data={itemData.item}
+      onSelectMeal={() => { }}
+    />
   }
   const catID = props.navigation.getParam('id')
   const displayedMeals = MEALS.filter(
@@ -23,6 +21,7 @@ const CategoryMealScreen = props => {
         data={displayedMeals}
         keyExtractor={item => item.id}
         renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   )
