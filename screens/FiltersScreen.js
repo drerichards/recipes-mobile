fe2fde5e1eb77d53a2851e61d3010954b4323985
A Richardson <g.andrerichards@gmail.com>
2020-01-31 03:17:56 -0500

    filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Switch } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../components/HeaderButton'
+import Colors from '../constants/Colors'
+
+const FilterSwitch = props => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text style={styles.text}>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.pink }}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  )
+}
 
 const FiltersScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false)
+  const [isLactoseFree, setIsLactoseFree] = useState(false)
+  const [isVegetarian, setIsVegetarian] = useState(false)
+  const [isVegan, setIsVegan] = useState(false)
+
   return (
     <View style={styles.screen}>
-      <Text>FiltersScreen</Text>
+      <Text style={styles.title}>Available Filters</Text>
+      <FilterSwitch
+        label={'Gluten Free'}
+        state={isGlutenFree}
+        onChange={newValue => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label={'Lactose Free'}
+        state={isLactoseFree}
+        onChange={newValue => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label={'Vegetarian'}
+        state={isVegetarian}
+        onChange={newValue => setIsVegetarian(newValue)}
+      />
+      <FilterSwitch
+        label={'Vegan'}
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)}
+      />
     </View>
   )
 }
@@ -28,8 +67,28 @@ FiltersScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center'
+  },
+  title: {
+    fontFamily: 'openSansBold',
+    fontSize: 20,
+    margin: 20,
+    textAlign: 'center'
+  },
+  filterContainer: {
+    width: '80%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  text: {
+    fontFamily: 'openSans'
   }
 })
 
