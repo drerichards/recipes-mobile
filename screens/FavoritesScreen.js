f71bc58bca28cbab837a3f56f4b92b81e14fe9de
A Richardson <g.andrerichards@gmail.com>
2020-02-01 10:50:28 -0500

    toggle fav screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { View, Text, StyleSheet } from 'react-native'
 import MealList from '../components/MealList'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../components/HeaderButton'
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals)
+
+  if (favMeals.length === 0 || !favMeals) {
+    return <View style={styles.screen}><Text style={styles.text}>No Favorites Found!</Text></View>
+  }
 
   return <MealList
     listData={favMeals}
@@ -25,5 +30,18 @@ FavoritesScreen.navigationOptions = navData => {
     </HeaderButtons>
   }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontFamily: 'openSansBold',
+    textAlign: 'center',
+    fontSize: 20
+  }
+})
 
 export default FavoritesScreen
