e5295019eeb6298164d1f6bdf0daff5eef0b0699
A Richardson <g.andrerichards@gmail.com>
2020-02-01 10:44:34 -0500

    toggle fav btn

--- components/MealList.js
+++ components/MealList.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { useSelector } from 'react-redux'
+import { View, StyleSheet, FlatList } from 'react-native'
 import MealItem from './MealItem'
 
 const MealList = props => {
+  const favoriteMeals = useSelector(state => state.meals.favoriteMeals)
+
   const renderMealItem = itemData => {
+    const isFavorite = favoriteMeals.find(meal =>
+      meal.id === itemData.item.id)
+
     return <MealItem data={itemData.item}
       onSelectMeal={() => {
         props.navigation.navigate({
           routeName: 'MealDetail',
           params: {
             mealId: itemData.item.id,
-            mealTitle: itemData.item.title
+            mealTitle: itemData.item.title,
+            isFav: isFavorite
           }
         })
       }}
